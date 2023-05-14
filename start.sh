4fe06f107cd7bceca0d2414d22bc9a80f9dd562f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-05-14 17:15:37 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/konichiwa55115/transtest /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+gunicorn app:app & python3 bot.py
