08b0bbb921c9ae92043dbb3eaf2876a4c1d9a746
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-16 08:00:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/vidmk /LazyDeveloper
+git clone https://github.com/GregorR/rnnoise-models  /LazyDeveloper
 cd /LazyDeveloper
-git clone https://github.com/GregorR/rnnoise-models
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 bot.py
