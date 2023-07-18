e465c94bca4077e1a36d113e5793f33bb04232ee
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-18 17:51:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def _telegram_file(client, message):
   global picture
   picture = "./picy/pic"
   global res 
-  cmd(f'ffmpeg -i {file_path} -af arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/"mod"+{mp3file} -y')
+  cmd(f'ffmpeg -i {file_path} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/"mod"+{mp3file} -y')
   cmd(f'ffmpeg -i ./downloads/"mod"+{mp3file} -af volume=2 ./downloads/{mp3file} -y ')
   cmd(f'ffmpeg -r 1 -loop 1 -y -i {picture} -i ./downloads/{mp3file} -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 {mp4file}')
 
@@ -86,7 +86,7 @@ def _telegram_file(client, message):
   mp4file="mp4file.mp4" 
   tempmp3 = "mod"+mp3file
   cmd(f'ffmpeg -i {file_path} -q:a 0 -map a ./downloads/{mp3file} -y')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -af arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y ')
+  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y ')
   cmd(f'ffmpeg -i ./downloads/{tempmp3} -af volume=2 ./downloads/{mp3file} -y ')
   cmd(f'ffmpeg -i {file_path} -i ./downloads/{mp3file} -c:v copy -map 0:v:0 -map 1:a:0 {mp4file} -y')
   with open(f'{mp4file}', 'rb') as f:
