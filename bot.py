8401c9515ddb0b5be75428b9e60b8984971ec4c1
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-20 00:40:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,8 +86,8 @@ def _telegram_file(client, message):
   mp4file="mp4file.mp4" 
   tempmp3 = "mod"+mp3file
   cmd(f'ffmpeg -i {file_path} -q:a 0 -map a ./downloads/{mp3file} -y')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -af volume=15dB ./downloads/{mp3file} -y ')
+  cmd(f'ffmpeg -i ./downloads/{mp3file} -af volume=15dB ./downloads/{tempmp3} -y ')
+  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
   cmd(f'ffmpeg -i {file_path} -i ./downloads/{mp3file} -c:v copy -map 0:v:0 -map 1:a:0 {mp4file} -y')
   with open(f'{mp4file}', 'rb') as f:
         bot.send_video(message.chat.id, f)
