700caa8239a5707f2035bca0cabb15712ad91e7b
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-20 11:32:37 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/vidmk /LazyDeveloper
-cd /LazyDeveloper
-git clone https://github.com/GregorR/rnnoise-models
-pip3 install -r requirements.txt
+cd /LazyDeveloper/vidmk
 echo "Starting Bot...."
 python3 bot.py
