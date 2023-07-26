279b465a05c571d35f70f8042aa409688fee97e8
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-26 12:49:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,112 +1,68 @@
-from pyrogram import Client, filters
-import subprocess
 import os
+from pyrogram import Client, filters
 from os import system as cmd
 import shutil
-
+from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 
 bot = Client(
-    "myfirs",
+    "voluemincreaser",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="6251349619:AAHWY6-_BIwHqTUzvH62ukVUThjohP13d5k"
+    bot_token="5442625381:AAEsUxtm8tthl0yDtHuFlZmdAQnqeY0IM94"
 )
+
+CHOOSE_UR_LANG = "اختر نمط التضخيم "
+CHOOSE_UR_LANG_BUTTONS = [
+    [InlineKeyboardButton("5db",callback_data="mod1")],
+     [InlineKeyboardButton("10db",callback_data="mod2")],
+     [InlineKeyboardButton("15db",callback_data="mod3")],
+     [InlineKeyboardButton("20db",callback_data="mod4")],
+     [InlineKeyboardButton("25db",callback_data="mod5")]
+]
+
+
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, "  السلام عليكم أنا بوت منتجة الفيديوهات . فقط أرسل التصميم ( الغلاف) بدون ضغط للحفاظ على جودة الفيديو \n\n Send without compression \n\n لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120",disable_web_page_preview=True)
-    
-@bot.on_message(filters.private & filters.incoming & filters.document | filters.photo)
-def _telegram_file(client, message):
-  if os.path.isdir("./downloads/") :
-        sent_message = message.reply_text('هناك عملية يتم الآن . أرسل الصوتية  بعد مدة من فضلك', quote=True)
-        return
-  else :
-        pass
-  user_id = message.from_user.id
-  sent_message = message.reply_text('الآن أرسل الصوتية', quote=True)
-  file = message.document
-  file_path = message.download(file_name="pic")
-  cmd(f'mkdir picy && mv ./downloads/pic ./picy')
-  global picture
-  pictrue="./picy/pic"
-  shutil.rmtree('./downloads/')
+    bot.send_message(message.chat.id, " السلام عليكم , أنا بوت تضخيم الصوتيات\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
 
-    
-    
-
-@bot.on_message(filters.private & filters.incoming & filters.audio | filters.voice )
+@bot.on_message(filters.private & filters.incoming &  filters.audio | filters.voice  )
 def _telegram_file(client, message):
-  if os.path.isdir("./downloads/") :
-        sent_message = message.reply_text('هناك عملية يتم الآن . أرسل الصوتية  بعد مدة من فضلك', quote=True)
-        return
-  else :
-        pass
-  user_id = message.from_user.id
-  sent_message = message.reply_text('[جار منتجة الفيديو', quote=True)
+  
+  global user_id
+  user_id = message.from_user.id 
   file = message.audio
   global file_path
-  file_path = message.download(file_name="aud")
-  global mp3file
-  mp3file = "mp3file.mp3"
-  global mp4file
-  mp4file="mp4file.mp4"
-  global picture
-  picture = "./picy/pic"
-  global tempmp3
-  tempmp3 = "mod"+mp3file
+  file_path = message.download(file_name="./downloads/")
+  global filename
+  filename = os.path.basename(file_path)
+  message.reply(
+             text = CHOOSE_UR_LANG,
+             reply_markup = InlineKeyboardMarkup(CHOOSE_UR_LANG_BUTTONS)
 
-  global res 
-  cmd(f'ffmpeg -i {file_path} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{tempmp3} -y')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -af volume=2 ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -r 1 -loop 1 -y -i {picture} -i ./downloads/{tempmp3} -c:v libx264 -tune stillimage -c:a copy -shortest -vf scale=1920:1080 {mp4file}')
-
-    # Upload transcription file to user
-  with open(f'{mp4file}', 'rb') as f:
-        bot.send_video(message.chat.id, f)
-  shutil.rmtree('./downloads/')
-  shutil.rmtree('./picy/')
-  cmd(f'rm {mp4file}')
-
-
+        )
+@bot.on_callback_query()
+def callback_query(CLIENT,CallbackQuery):
+  global langtoken
+  if CallbackQuery.data == "mod1":
+      langtoken = 5
+  elif CallbackQuery.data == "mod2":
+      langtoken = 10
+  elif CallbackQuery.data == "mod3":
+      langtoken = 15
+  elif CallbackQuery.data == "mod4" :
+      langtoken = 20
+  elif CallbackQuery.data == "mod5":
+      langtoken = 25
   
-
-@bot.on_message(filters.private & filters.incoming & filters.video )
-def _telegram_file(client, message):
-  if os.path.isdir("./downloads/") :
-        sent_message = message.reply_text('هناك عملية يتم الآن . أرسل التصميم  بعد مدة من فضلك', quote=True)
-        return
-  else :
-        pass
-  
-  user_id = message.from_user.id
-  sent_message = message.reply_text('[جار منتجة الفيديو', quote=True)
-  file = message.audio
-  global file_path
-  file_path = message.download(file_name="aud")
-  global mp3file
-  mp3file = "mp3file.mp3"
-  global mp4file
-  mp4file="mp4file.mp4" 
-  tempmp3 = "mod"+mp3file
-  cmd(f'ffmpeg -i {file_path} -q:a 0 -map a ./downloads/{mp3file} -y')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -af volume=15dB ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/somnolent-hogwash-2018-09-01/sh.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/beguiling-drafter-2018-08-30/bd.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/conjoined-burgers-2018-08-28/cb.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i ./downloads/{mp3file} -vf arnndn=m=./rnnoise-models/leavened-quisling-2018-08-31/lq.rnnn ./downloads/{tempmp3} -y ')
-  cmd(f'ffmpeg -i ./downloads/{tempmp3} -vf arnndn=m=./rnnoise-models/marathon-prescription-2018-08-29/mp.rnnn ./downloads/{mp3file} -y ')
-  cmd(f'ffmpeg -i {file_path} -i ./downloads/{mp3file} -c:v copy -map 0:v:0 -map 1:a:0 {mp4file} -y')
-  with open(f'{mp4file}', 'rb') as f:
-        bot.send_video(message.chat.id, f)
-  shutil.rmtree('./downloads/')
-  cmd(f'rm {mp4file}')
-
-
+  CallbackQuery.edit_message_text(
+      
+      "جار التضخيم"
+  )   
+  cmd(f'''ffmpeg -i "{file_path}" -filter:a volume={langtoken}dB "{filename}"''')
+  with open(filename, 'rb') as f:
+        bot.send_audio(user_id, f)
+  shutil.rmtree('./downloads/') 
+  cmd(f'''unlink "{filename}"''')
 
 
 
