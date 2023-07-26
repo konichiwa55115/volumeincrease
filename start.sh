4342eea3e33505b677b50f6d09e83fdc4feaa98a
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-07-26 12:55:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-cd /LazyDeveloper/vidmk
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/volumeincrease /kony
+cd /kony
+pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+gunicorn app:app & python3 bot.py
